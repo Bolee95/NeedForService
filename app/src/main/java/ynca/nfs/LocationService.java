7dcb649241ca467865608f79ae394b720cb59ff3
djuralfc <djurovicnis@gmail.com>
2018-08-13 00:33:16 +0200

    Gotov background service

--- app/src/main/java/ynca/nfs/LocationService.java
+++ app/src/main/java/ynca/nfs/LocationService.java
@@ -1,5 +1,6 @@
 package ynca.nfs;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -9,31 +10,53 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import ynca.nfs.Activities.mainScreensActivities.mainScreenClientActivity;
 import ynca.nfs.Models.Client;
+import ynca.nfs.Models.VehicleService;
 
 public class LocationService extends Service {
 
+    //TODO gasenje servisa u opcijama i prilikom odjavljivanja?
     //private static final String TAG = "Lokacija";
     private LocationManager mLocationManager = null;
     private static final int LOCATION_INTERVAL = 1000;
     private static final float LOCATION_DISTANCE = 0;
+    private static final float CLIENT_SERVICE_DISTANCE = 100;
+
     private Location mLastClientLocation;
-    private Location mVehicleServiceLocation;
+    private Location mServiceLocation;
     private Client currentClient;
+    private ArrayList<VehicleService> mVehicleServices;
+    private VehicleService toRemove; //kada se pojavi notifikacija za neki servis, taj servis se dodaje ovde i on se brise; da se ne bi ponovo pojavila notifikacija
+    private Map<String,Object> childUpdate; //koristi se za update lokacije
+    private ChildEventListener mChildEventListener;
+
+    private NotificationCompat.Builder mBuilder;
 
     private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-
+    private DatabaseReference mDatabaseReference; //za klijente
+    private DatabaseReference mDatabaseReference2; //za servise
 
 
     /*
@@ -70,11 +93,29 @@ public class LocationService extends Service {
             currentClient.setLastKnownLat(mLastClientLocation.getLatitude());
             currentClient.setLastKnownlongi(mLastClientLocation.getLongitude());
 
-            Map<String,Object> childUpdate = new HashMap<>();
             childUpdate.put("lastKnownLat",mLastClientLocation.getLatitude());
             childUpdate.put("lastKnownlongi",mLastClientLocation.getLongitude());
             //upis u bazu
             mDatabaseReference.child(currentClient.getUID()).updateChildren(childUpdate);
+
+            for (VehicleService vs : mVehicleServices) {
+                mServiceLocation.setLatitude(vs.getLat());
+                mServiceLocation.setLongitude(vs.getLongi());
+
+                if (mLastClientLocation.distanceTo(mServiceLocation) < CLIENT_SERVICE_DISTANCE) {
+                    //Toast.makeText(getApplicationContext(), "Servis je u blizini!", Toast.LENGTH_SHORT).show();
+
+                    //notifikacija
+                    mBuilder.setContentTitle("Auto servis " + vs.getName() + " se nalazi u vasoj blizini");
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                    notificationManager.notify(1, mBuilder.build());
+
+                    toRemove = vs;
+
+                }
+            }
+            mVehicleServices.remove(toRemove);
         }
 
         @Override
@@ -118,7 +159,11 @@ public class LocationService extends Service {
     @Override
     public void onCreate() {
         mLastClientLocation = new Location("");
-        mVehicleServiceLocation = new Location("");
+        mServiceLocation = new Location("");
+        currentClient = new Client();
+        mVehicleServices = new ArrayList<>();
+        toRemove = new VehicleService();
+        childUpdate = new HashMap<>();
         initializeLocationManager();
 
         //uzimanje ulogovanog klijenta
@@ -130,6 +175,49 @@ public class LocationService extends Service {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference().child("Korisnik").child("Client");
+        mDatabaseReference2 = mFirebaseDatabase.getReference().child("Korisnik").child("VehicleService");
+
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                VehicleService vs = dataSnapshot.getValue(VehicleService.class);
+                mVehicleServices.add(vs);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        mDatabaseReference2.addChildEventListener(mChildEventListener);
+
+        //builder za notifikaciju
+        Intent intent = new Intent(getApplicationContext(), mainScreenClientActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+        mBuilder = new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.sport_car_logos) //TODO potrebna je ikonica u samo beloj boji
+                .setContentText("Klikom na notifikaciju otvoricete aplikaciju.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
 
         /*
         try {
@@ -153,6 +241,7 @@ public class LocationService extends Service {
                 //Log.i(TAG, "fail to remove location listners, ignore", ex);
             }
         }
+        mDatabaseReference2.removeEventListener(mChildEventListener);
     }
 
     private void initializeLocationManager() {
